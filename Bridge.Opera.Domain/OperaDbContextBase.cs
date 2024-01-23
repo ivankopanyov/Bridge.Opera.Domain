65493707ee692e605db2c98da7f56ad1d7a9572e
ivankopanyov <i.kopanyov@gmail.com>
2024-01-23 16:57:09 +0300

    Added Bridge.Opera.Domain

---
+++ Bridge.Opera.Domain/OperaDbContextBase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Bridge.Opera.Domain
+{
+    public abstract class OperaDbContextBase : DbContext
+    {
+        private static ISet<Type> _types;
+
+        private static IEnumerable<MethodInfo> _methods;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("OPERA");
+
+            if (_types == null)
+                _types = GetType().GetProperties()
+                    .Where(property => property.PropertyType.Name == "DbSet`1" &&
+                        property.PropertyType.GenericTypeArguments.Any())
+                    .Select(property => property.PropertyType.GenericTypeArguments.First())
+                    .ToHashSet();
+
+            if (_methods == null)
+                _methods = _types
+                    .SelectMany(type => type.GetMethods()
+                        .Where(method => method.Name == nameof(OnModelCreating) &&
+                            method.IsStatic &&
+                            method.GetParameters().Length == 2 &&
+                            method.GetParameters().Any(parameter => parameter.ParameterType == typeof(ModelBuilder)) &&
+                            method.GetParameters().Any(parameter => parameter.ParameterType == typeof(ISet<Type>))));
+
+            foreach (var method in _methods)
+                method.Invoke(null, new object[] { modelBuilder, _types });
+        }
+    }
+}
